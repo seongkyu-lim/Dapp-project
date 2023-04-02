bbd508b4f1a501451dfa635bf997fcbd707e1a32
limseongkyu <96x60812@gmail.com>
2023-04-02 14:00:05 +0900

    Feature: my-animal page

--- frontend/src/routes/my-animal.tsx
+++ frontend/src/routes/my-animal.tsx
@@ -1,11 +1,46 @@
-import React, { FC } from "react";
+import { Grid } from "@chakra-ui/react";
+import React, { FC, useEffect, useState } from "react";
+import AnimalCard from "../components/AnimalCard";
+import { mintAnimalTokenContract } from "../contracts";
 
 interface MyAnimalProps {
     account: string;
 }
 
-const MyAnimal: FC<MyAnimalProps> = () => {
-    return <div>MA</div>
-}
+const MyAnimal: FC<MyAnimalProps> = ({account}) => {
+    const [animalCardArray, setAnimalCardArray] = useState<string[]>();
+    const getAnimalToken = async () => {
+        try{
+            const balanceLength = await mintAnimalTokenContract.methods.balanceOf(account).call();
+
+            const tempAnimalCardArray = [];
+
+            for(let i = 0; i < parseInt(balanceLength, 10); i++){
+                const animalTokenId = await mintAnimalTokenContract.methods.tokenOfOwnerByIndex(account, i).call();
+                const animalType = await mintAnimalTokenContract.methods.animalTypes(animalTokenId).call();
+                tempAnimalCardArray.push(animalType);
+            }
+            setAnimalCardArray(tempAnimalCardArray);
+        }catch(err){
+            console.error(err);
+        }
+    }
+    useEffect(() => {
+        if(!account) return;
+        getAnimalToken();
+    }, [account]);
+
+    useEffect(() =>{
+        console.log(animalCardArray);
+    }, [animalCardArray]);
+
+    return (
+        <Grid templateColumns="repeat(4, 1fr)" gap={8} >
+            {animalCardArray && animalCardArray.map((v, i) => {
+                    return <AnimalCard key={i} animalType={v} />
+                })};
+        </Grid>
+    );
+};
 
 export default MyAnimal;
